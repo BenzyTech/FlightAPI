72dcafd1553e68c24ff27d392ef2c8e99994f2eb
BenzyTech <50049208+BenzyTech@users.noreply.github.com>
2022-11-04 16:11:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { json } = require("express");
+const flights = require("./controllers/flightController");
+const models = require("./models/Flight");
+const routes = require("./routes/flightRoute");
+
+const app = express();
+
+app.use(json());
+
+app.use("/", routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+var users = [{
+    id: "1",
+    name: "benzy"
+
+}]
+
+app.get('../users', (reg, res) => {
+    res.sen({
+        success: true,
+        mesaage: 'data fatched Succesfully',
+        data: users
+    })
+
+})
